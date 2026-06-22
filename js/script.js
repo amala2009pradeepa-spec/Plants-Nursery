2942167bf5fc787b5f0af6b522faff34c751643d
Amala <your-email@example.com>
2026-06-22 22:49:33 +0530

    Add project files

---
+++ js/script.js
@@ -0,0 +1,17 @@
+let slides = document.querySelectorAll(".slides img");
+
+let current = 0;
+
+setInterval(() => {
+
+slides[current].classList.remove("active");
+
+current++;
+
+if(current >= slides.length){
+current = 0;
+}
+
+slides[current].classList.add("active");
+
+},3000);
